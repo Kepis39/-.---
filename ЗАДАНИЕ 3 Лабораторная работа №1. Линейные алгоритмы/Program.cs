35d9fd94d06f1341198062ab9d38cb123044f023
Kepis39 <101018161+Kepis39@users.noreply.github.com>
2025-10-05 18:48:23 +0200

    Delete ЗАДАНИЕ 3 Лабораторная работа №1. Линейные алгоритмы directory

--- ЗАДАНИЕ 3 Лабораторная работа №1. Линейные алгоритмы/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿//Целочисленная арифметика.Задание 3.33
-
-Console.WriteLine("Введите трехзначное число");
-int x = int.Parse(Console.ReadLine());
-int LastDigit = x % 10;
-int n =  LastDigit * 100 +(x - LastDigit)/10;
-Console.WriteLine($"n = {n}");
-
