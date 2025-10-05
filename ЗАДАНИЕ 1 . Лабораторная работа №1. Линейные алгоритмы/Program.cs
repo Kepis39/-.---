8817515fd8ece0d8fafd5c417abe6681bdfd640c
Kepis39 <101018161+Kepis39@users.noreply.github.com>
2025-10-05 18:48:07 +0200

    Delete ЗАДАНИЕ 1 . Лабораторная работа №1. Линейные алгоритмы directory

--- ЗАДАНИЕ 1 . Лабораторная работа №1. Линейные алгоритмы/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿//24 Вариант средний уровень(Арифметические выражения)
-
-
-Console.WriteLine("Введите y: ");
-double y = double.Parse(Console.ReadLine());
-Console.WriteLine("Введите k: ");
-double k = double.Parse(Console.ReadLine());
-Console.WriteLine($"U ={(Math.Log(2*k+4.3))/(Math.Exp(k+y)) + Math.Sqrt(y)} ");
-
